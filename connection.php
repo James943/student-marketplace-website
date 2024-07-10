7ff8e622c80178d611423e8a5a1c40cf217e7546
James <80528511+James943@users.noreply.github.com>
2024-07-10 15:31:54 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php      
     $host = "localhost";  
-    $user = "jn494_james";  
-    $password = '=.4KG#v^Pjc9';  
-    $db_name = "jn494_ci601";  
+    $user = "****";  
+    $password = '****';  
+    $db_name = "****";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
